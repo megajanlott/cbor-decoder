4d7d06005c028f87e8923e46e65204ce8f99d109
Viktor Ban <viktor.ban@gmail.com>
2017-03-31 08:38:54 +0200

    type hinting

--- cbor/CBORStream.py
+++ cbor/CBORStream.py
@@ -1,9 +1,12 @@
+from io import RawIOBase
+
+
 class CBORStream:
-    def __init__(self, stream):
+    def __init__(self, stream: RawIOBase):
         self.stream = stream
 
-    def peek(self, n=1):
+    def peek(self, n: int = 1):
         pass
 
-    def read(self, n=1):
+    def read(self, n: int = 1):
         return self.stream.read(n)
--- cbor/HexBinStream.py
+++ cbor/HexBinStream.py
@@ -1,14 +1,15 @@
-from cbor.CBORStream import CBORStream
 from binascii import unhexlify
+from cbor.CBORStream import CBORStream
+from io import RawIOBase
 
 
 class HexBinStream(CBORStream):
-    def __init__(self, stream):
+    def __init__(self, stream: RawIOBase):
         super().__init__(stream)
 
-    def peek(self, n=1):
+    def peek(self, n: int = 1):
         pass
 
-    def read(self, n=1):
+    def read(self, n: int = 1):
         in_hex = self.stream.read(2*n)
         return unhexlify(in_hex)
