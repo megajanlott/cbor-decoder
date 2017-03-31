620889a23c236d26d46857820a97872f072ca652
Viktor Ban <viktor.ban@gmail.com>
2017-03-31 09:41:43 +0200

    better peeky

--- cbor/CBORStream.py
+++ cbor/CBORStream.py
@@ -4,19 +4,23 @@ from io import RawIOBase
 class CBORStream:
     def __init__(self, stream: RawIOBase):
         self.stream = stream
-        self.peeked = None
-        self.peek_length = 1
+        self.peeked = b''
 
-    def peek1(self):
-        self.peeked = self.read(1)
+    def peek(self, n: int = 1):
+        if len(self.peeked) >= n:
+            return self.peeked[:n]
+        self.peeked += self._read(n - len(self.peeked))
         return self.peeked
 
     def read(self, n: int = 1):
         output = bytes()
-        if self.peeked is not None:
-            output = bytes(self.peeked)
-            self.peeked = None
-            if n >= self.peek_length:
-                n -= self.peek_length
-        output += bytes(self.stream.read(n))
+        if len(self.peeked) > 0:
+            output = self.peeked
+            if n >= len(self.peeked):
+                n -= len(self.peeked)
+            self.peeked = b''
+        output += self._read(n)
         return output
+
+    def _read(self, n: int):
+        return bytes(self.stream.read(n))
--- cbor/HexBinStream.py
+++ cbor/HexBinStream.py
@@ -6,8 +6,7 @@ from io import RawIOBase
 class HexBinStream(CBORStream):
     def __init__(self, stream: RawIOBase):
         super().__init__(stream)
-        self.peek_length = 2
 
-    def read(self, n: int = 1):
+    def _read(self, n: int):
         in_hex = self.stream.read(2*n)
-        return bytes(unhexlify(in_hex))
+        return unhexlify(in_hex)
