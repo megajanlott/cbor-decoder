f16f011de3d6a1484c2d99d983203a42d12da70a
vgellar <solarian.vg@gmail.com>
2017-03-31 09:18:31 +0200

    stream-, and array wrapping

--- cbor/Decoder.py
+++ cbor/Decoder.py
@@ -1,16 +1,24 @@
 from cbor.CBORStream import CBORStream
-from io import RawIOBase
+from cbor.HexBinStream import HexBinStream
+from io import RawIOBase, BytesIO
 
 
 class Decoder:
     def decode(self, array: bytes):
-        pass
+        decode_stream = CBORStream(BytesIO(array))
+        return self.__decode(decode_stream)
 
     def decode(self, stream: RawIOBase):
-        pass
+        decode_stream = CBORStream(stream)
+        return self.__decode(decode_stream)
 
     def decode_hex(self, array: bytes):
-        pass
+        decode_stream = HexBinStream(BytesIO(array))
+        return self.__decode(decode_stream)
 
     def decode_hex(self, stream: RawIOBase):
+        decode_stream = HexBinStream(stream)
+        return self.__decode(decode_stream)
+
+    def __decode(self, stream: CBORStream):
         pass
