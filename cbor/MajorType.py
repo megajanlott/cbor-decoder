b387290d8fb5892ac0577ca482085c96fcdad8c4
Barnabas Gema <barnabas.gema@zooshgroup.com>
2017-04-10 14:56:52 +0200

    tests MajorType.py

--- cbor/MajorType.py
+++ cbor/MajorType.py
@@ -12,8 +12,10 @@ class MajorType(State):
         t = (ord(current) & MAJOR_TYPE_MASK) >> (8 - MAJOR_TYPE_SIZE)
 
         if t == 0:
+            # should return the proper major type instance
             return 'Unsigned int'
         elif t == 1:
+            # should return the proper major type instance
             return 'Negative int'
 
         return 'No type'
--- tests/test_Stack.py
+++ tests/test_Stack.py
@@ -3,51 +3,51 @@ import sys
 import pytest
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
 
-from cbor import Stack
+from cbor.Stack import Stack
 
 
 def test_init():
-    stack = Stack.Stack()
+    stack = Stack()
     assert stack.items == []
 
 def test_isEmpty():
-    stack = Stack.Stack()
+    stack = Stack()
     assert stack.isEmpty() == True
-    stack = Stack.Stack()
+    stack = Stack()
     stack.push(10)
     assert stack.isEmpty() == False
 
 def test_push():
-    stack = Stack.Stack()
+    stack = Stack()
     stack.push(5)
     assert stack.items == [5]
     stack.push('test')
     assert stack.items == [5, 'test']
 
 def test_push_array():
-    stack = Stack.Stack()
+    stack = Stack()
     stack.push([3, 4])
     assert stack.items == [3, 4]
     stack.push(['test', [5, 6]])
     assert stack.items == [3, 4, 'test', [5, 6]]
 
 def test_pop():
-    stack = Stack.Stack()
+    stack = Stack()
     stack.push([1, 3, 4])
     assert stack.pop() == 4
     assert stack.items == [1, 3]
-    stack = Stack.Stack()
+    stack = Stack()
     with pytest.raises(Exception):
         stack.pop()
 
 def test_peek():
-    stack = Stack.Stack()
+    stack = Stack()
     stack.push([1, 3, 4])
     assert stack.peek() == 4
     assert stack.items == [1, 3, 4]
 
 def test_size():
-    stack = Stack.Stack()
+    stack = Stack()
     stack.push(5)
     assert stack.size() == 1
     stack.push([6, 7])
