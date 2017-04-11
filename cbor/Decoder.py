9441f705cd647005d0a379b50608ecdfab3e0ca4
gellert <gellihegyi@gmail.com>
2017-04-11 22:23:34 +0200

    adds handler parameters to decoder methods

--- cbor/Decoder.py
+++ cbor/Decoder.py
@@ -5,22 +5,22 @@ from cbor.MajorType import MajorType
 
 
 class Decoder:
-    def decode_array(self, array: bytes):
+    def decode_array(self, array: bytes, handler):
         decode_stream = CBORStream(BytesIO(array))
-        return self.__decode(decode_stream)
+        return self.__decode(decode_stream, handler)
 
-    def decode_stream(self, stream: RawIOBase):
+    def decode_stream(self, stream: RawIOBase, handler):
         decode_stream = CBORStream(stream)
-        return self.__decode(decode_stream)
+        return self.__decode(decode_stream, handler)
 
-    def decode_hex_array(self, array: bytes):
+    def decode_hex_array(self, array: bytes, handler):
         decode_stream = HexBinStream(BytesIO(array))
-        return self.__decode(decode_stream)
+        return self.__decode(decode_stream, handler)
 
-    def decode_hex_stream(self, stream: RawIOBase):
+    def decode_hex_stream(self, stream: RawIOBase, handler):
         decode_stream = HexBinStream(stream)
-        return self.__decode(decode_stream)
+        return self.__decode(decode_stream, handler)
 
-    def __decode(self, stream: CBORStream):
+    def __decode(self, stream: CBORStream, handler):
         mt = MajorType()
         return mt.run(stream, None)
--- examples/simple.py
+++ examples/simple.py
@@ -5,8 +5,13 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
 
 from cbor.Decoder import Decoder
 
+
+def printer(data):
+    print("Received:", data)
+
+
 if __name__ == '__main__':
     d = Decoder()
     data = bytes([3, 4, 5])
     print(BytesIO(data))
-    print("Decoded:", d.decode_array(data))
+    print("Decoded:", d.decode_array(data, printer))
