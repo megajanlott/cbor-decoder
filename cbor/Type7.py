931cf789c9ebb58f4654f12b0705ea004defcc7d
tnagy <tnagy@rdisoftware.com>
2017-04-12 01:33:13 +0200

    Major Type 7 - Floats & Simple Values
    Type 7 tests

---
+++ cbor/Type7.py
@@ -0,0 +1,86 @@
+from cbor.CBORStream import CBORStream
+from cbor.State import State
+
+ADDITIONAL_INFORMATION_MASK = 0b00011111
+
+simple_value_next = 24
+float_2byte_next = 25
+float_4byte_next = 26
+float_8byte_next = 27
+inf_end = 31
+
+
+def decode_simple_value(value):
+    if value <=19:
+        #unassigned
+        return 'pass'
+    elif value == 20:
+        return 'False'
+    elif value == 21:
+        return 'True'
+    elif value == 22:
+        return 'Null'
+    elif value == 23:
+        #undefined
+        return 'pass'
+    elif ((value>=24) & (value<=31)):
+        #reserved
+        return 'pass'
+    else:
+        #unassigned
+        return 'pass'
+
+class Type7Info(State):
+
+    def run(self, stream: CBORStream, handler):
+        current = stream.read(1)
+
+        add_inf = (current & ADDITIONAL_INFORMATION_MASK)
+
+        if add_inf < 24:
+            simple_value = decode_simple_value(add_inf)
+            handler(simple_value)
+            return None
+        elif add_inf == simple_value_next:
+            return Type7Read
+        elif add_inf == float_2byte_next:
+            return FloatRead(2)
+        elif add_inf == float_4byte_next:
+            return FloatRead(4)
+        elif add_inf == float_8byte_next:
+            return FloatRead(8)
+        elif add_inf == inf_end:
+            return 'break'
+
+    def type(self):
+        raise NotImplementedError
+
+
+class Type7Read(State):
+
+    def run(self, stream: CBORStream, handler):
+        value = stream.read(1)
+
+        simple_value = decode_simple_value(value)
+        handler(simple_value)
+        return None
+
+    def type(self):
+        raise NotImplementedError
+
+
+class FloatRead(State):
+
+    def __init__(self, n):
+        self.bytes_to_read = n
+
+    def run(self, stream: CBORStream, handler):
+        value = stream.read(self.bytes_to_read)
+        float.fromhex(value)
+
+        float_value = decode_simple_value(value)
+        handler(float_value)
+        return None
+
+    def type(self):
+        raise NotImplementedError
