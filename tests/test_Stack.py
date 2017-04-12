b4b879cc69e844952d53ae8faf42423b3cab52cd
Barnabas Gema <barnabas.gema@zooshgroup.com>
2017-04-12 03:14:18 +0200

    removes accidental merge residue

--- tests/test_Stack.py
+++ tests/test_Stack.py
@@ -10,14 +10,6 @@ def test_init():
     stack = Stack()
     assert stack.items == []
 
-<<<<<<< HEAD
-def test_isEmpty():
-    stack = Stack()
-    assert stack.isEmpty() == True
-    stack = Stack()
-    stack.push(10)
-    assert stack.isEmpty() == False
-=======
 
 def test_isEmpty():
     stack = Stack()
@@ -26,7 +18,6 @@ def test_isEmpty():
     stack.push(10)
     assert stack.isEmpty() is False
 
->>>>>>> adds E251 to ignored errors and makes tests pass the linter
 
 def test_push():
     stack = Stack()
@@ -35,10 +26,7 @@ def test_push():
     stack.push('test')
     assert stack.items == [5, 'test']
 
-<<<<<<< HEAD
-=======
 
->>>>>>> adds E251 to ignored errors and makes tests pass the linter
 def test_push_array():
     stack = Stack()
     stack.push([3, 4])
@@ -46,10 +34,7 @@ def test_push_array():
     stack.push(['test', [5, 6]])
     assert stack.items == [3, 4, 'test', [5, 6]]
 
-<<<<<<< HEAD
-=======
 
->>>>>>> adds E251 to ignored errors and makes tests pass the linter
 def test_pop():
     stack = Stack()
     stack.push([1, 3, 4])
@@ -59,20 +44,14 @@ def test_pop():
     with pytest.raises(Exception):
         stack.pop()
 
-<<<<<<< HEAD
-=======
 
->>>>>>> adds E251 to ignored errors and makes tests pass the linter
 def test_peek():
     stack = Stack()
     stack.push([1, 3, 4])
     assert stack.peek() == 4
     assert stack.items == [1, 3, 4]
 
-<<<<<<< HEAD
-=======
 
->>>>>>> adds E251 to ignored errors and makes tests pass the linter
 def test_size():
     stack = Stack()
     stack.push(5)
