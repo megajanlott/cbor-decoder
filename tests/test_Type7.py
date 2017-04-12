e83be9dbaa595450d9102674280e4cd97c3277d6
Barnabas Gema <barnabas.gema@zooshgroup.com>
2017-04-12 03:03:27 +0200

    fixes tests and lint (partly)

--- tests/test_Type7.py
+++ tests/test_Type7.py
@@ -8,14 +8,27 @@ from cbor.MajorType import MajorType
 from cbor.CBORStream import CBORStream
 
 
+def assertHandler(value):
+    def handler(v):
+        assert v == value
+    return handler
+
+
 def test_Type7Info():
     data = CBORStream(BytesIO(bytes([0b11111000])))
-    assert Type7Info().run(data, None) == Type7Read
+    assert type(Type7Info().run(data, None)) == Type7Read
+
     data = CBORStream(BytesIO(bytes([0b11111111])))
     assert Type7Info().run(data, None) == 'break'
+
     data = CBORStream(BytesIO(bytes([0b11111001])))
-    assert Type7Info().run(data, None) == FloatRead(2)
+    result = Type7Info().run(data)
+    assert type(result) == FloatRead
+    assert result.bytes_to_read == 2
+
 
 def test_Type7Read():
     data = CBORStream(BytesIO(bytes([0b11110100])))
-    assert Type7Read().run(data, None) == None
+    handler = assertHandler('pass')
+    result = Type7Read().run(data, handler)
+    assert result is None
