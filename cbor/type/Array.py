b4ac3bd21d3d44b708a9b2c6cbcfe492f647df1d
Viktor Ban <viktor.ban@gmail.com>
2017-04-12 13:28:16 +0200

    Expand skeleton

--- cbor/type/Array.py
+++ cbor/type/Array.py
@@ -1,37 +1,52 @@
+from cbor.CBORStream import CBORStream
 from cbor.MajorType import MajorType
 
 
 class ArrayInfo(MajorType):
 
-    def type(self):
+    def __init__(self):
         pass
 
-    def __init__(self):
+    def run(self, stream: CBORStream, handler):
+        pass
+
+    def type(self):
         pass
 
 
 class ArrayRead(MajorType):
 
-    def type(self):
+    def __init__(self, n: int):
+        self.n = n
         pass
 
-    def __init__(self, n : int):
+    def run(self, stream: CBORStream, handler):
+        pass
+
+    def type(self):
         pass
 
 
 class ArrayLen(MajorType):
 
-    def type(self):
+    def __init__(self, n: int):
+        self.n = n
         pass
 
-    def __init__(self, n : int):
+    def run(self, stream: CBORStream, handler):
+        pass
+
+    def type(self):
         pass
 
 
 class ArrayInf(MajorType):
 
-    def type(self):
+    def __init__(self):
         pass
 
-    def __init__(self):
+    def run(self, stream: CBORStream, handler):
+        pass
+
+    def type(self):
         pass
