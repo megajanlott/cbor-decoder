4c8078634e05fc3f454317a887592707a2390f71
Viktor Ban <viktor.ban@gmail.com>
2017-04-12 14:59:10 +0200

    ArrayInfo

--- cbor/type/Array.py
+++ cbor/type/Array.py
@@ -1,11 +1,25 @@
 from cbor.CBORStream import CBORStream
+from cbor.MajorType import MajorType
 from cbor.State import State
 
 
 class ArrayInfo(State):
 
     def run(self, stream: CBORStream, handler):
-        pass
+        info = stream.read(1)
+        length = ord(info) & 0b00011111
+        if length < 24:
+            return [MajorType(), ArrayRead(length)]
+        elif length == 24:
+            return [ArrayLen(1)]
+        elif length == 25:
+            return [ArrayLen(2)]
+        elif length == 26:
+            return [ArrayLen(4)]
+        elif length == 27:
+            return [ArrayLen(8)]
+        elif length == 31:
+            return [MajorType(), ArrayInf()]
 
 
 class ArrayRead(State):
