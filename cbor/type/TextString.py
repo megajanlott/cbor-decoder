56f8969b9bcb3513913caea4fddc68d1c733c889
hoooman <solarian.vg@gmail.com>
2017-04-17 21:09:53 +0200

    initial strings

---
+++ cbor/type/TextString.py
@@ -0,0 +1,25 @@
+import cbor.CBORStream
+import cbor.MajorType
+import cbor.State
+
+
+class TextString(cbor.State.State):
+
+    def run(self, stream: cbor.CBORStream.CBORStream, handler):
+        info = stream.read(1)
+        length = ord(info) & 0b00011111
+        if length == 0:
+            handler('')
+        elif length < 24:
+            pass
+        elif length == 24:
+            pass
+        elif length == 25:
+            pass
+        elif length == 26:
+            pass
+        elif length == 27:
+            pass
+        elif length == 31:
+            pass
+        return []
