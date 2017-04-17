1e73739804c6dc1c5b8f520bd114bb975cca63f9
tadam <kidama62@gmail.com>
2017-04-17 22:46:04 +0200

    uint, negint dev version done

--- cbor/neg_int.py
+++ cbor/neg_int.py
@@ -11,31 +11,31 @@ NEXT_4BYTE = 26
 NEXT_8BYTE = 27
 
 
-def decode_uint(data, length):
+def decode_neg_int(data, length):
     print(data)
     if length == 1:
-        return struct.unpack('B', data)[0]
+        return -1 - (struct.unpack('B', data)[0])
     elif length == 2:
         numbers = struct.unpack('BB', data)
-        return (256 * numbers[0] + numbers[1])
+        return -1 - (256 * numbers[0] + numbers[1])
     elif length == 4:
         numbers = struct.unpack('BBBB', bytes)
-        return (16777216 * numbers[0] + 65536 * numbers[1] + 256 * numbers[2] + numbers[3])
+        return -1 - (16777216 * numbers[0] + 65536 * numbers[1] + 256 * numbers[2] + numbers[3])
     elif length == 8:
         numbers = struct.unpack('BBBBBBBB', bytes)
-        return (72057594037927936 * numbers[0] + 281474976710656 * numbers[1] + 1099511627776 * numbers[2] + 4294967296 * numbers[3] + \
+        return -1 - (72057594037927936 * numbers[0] + 281474976710656 * numbers[1] + 1099511627776 * numbers[2] + 4294967296 * numbers[3] + \
         16777216 * numbers[4] + 65536 * numbers[5] + 256 * numbers[6] + numbers[7])
 
 
-class UIntRead(State):
+class NegIntRead(State):
     def __init__(self, n):
         self.n = n
         self.run(CBORStream)
 
     def run(self, stream: CBORStream, handler=None):
-        current_data = bytes([1,244])  # must be changed to read from stream
+        current_data = bytes([1,243])  # must be changed to read from stream
 
-        data = decode_uint(current_data, self.n)
+        data = decode_neg_int(current_data, self.n)
         print(data)
         # handler(data)
         return None
@@ -44,7 +44,7 @@ class UIntRead(State):
         raise NotImplementedError
 
 
-class UIntInfo(State):
+class NegIntInfo(State):
     def run(self, stream: CBORStream, handler=None):
 
         current_data = bytes([25])  # must be changed to read from stream
@@ -58,16 +58,16 @@ class UIntInfo(State):
             return None
         elif last_five_bits == NEXT_1BYTE:
             print('1byte')
-            return UIntRead(1)
+            return NegIntRead(1)
         elif last_five_bits == NEXT_2BYTE:
             print('2byte')
-            return UIntRead(2)
+            return NegIntRead(2)
         elif last_five_bits == NEXT_4BYTE:
             print('4byte')
-            return UIntRead(4)
+            return NegIntRead(4)
         elif last_five_bits == NEXT_8BYTE:
             print('8byte')
-            return UIntRead(8)
+            return NegIntRead(8)
         else:
             return None
 
@@ -75,5 +75,5 @@ class UIntInfo(State):
         raise NotImplementedError
 
 
-UInt = UIntInfo()
+UInt = NegIntInfo()
 UInt.run(CBORStream)
