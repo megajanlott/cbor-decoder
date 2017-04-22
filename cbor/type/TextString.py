d1f527cc91511aa17a073a0e08faaa9535ccfc98
hoooman <solarian.vg@gmail.com>
2017-04-22 15:50:55 +0200

    text string

--- cbor/type/TextString.py
+++ cbor/type/TextString.py
@@ -3,6 +3,14 @@ import cbor.MajorType
 import cbor.State
 
 
+def int_length(info: bytes):
+    return int.from_bytes(info, byteorder='big')
+
+
+def unicode_decode_bytes(info: bytes):
+    return info.decode("utf-8")
+
+
 class TextString(cbor.State.State):
 
     def run(self, stream: cbor.CBORStream.CBORStream, handler):
@@ -11,15 +19,22 @@ class TextString(cbor.State.State):
         if length == 0:
             handler('')
         elif length < 24:
-            pass
+            handler(unicode_decode_bytes(stream.read(length)))
         elif length == 24:
-            pass
+            handler(unicode_decode_bytes(stream.read(int_length(stream.read(1)))))
         elif length == 25:
-            pass
+            handler(unicode_decode_bytes(stream.read(int_length(stream.read(2)))))
         elif length == 26:
-            pass
+            handler(unicode_decode_bytes(stream.read(int_length(stream.read(3)))))
         elif length == 27:
-            pass
+            handler(unicode_decode_bytes(stream.read(int_length(stream.read(4)))))
         elif length == 31:
-            pass
+            return [cbor.MajorType.MajorType(), TextStringInf()]
         return []
+
+
+class TextStringInf(cbor.State.State):
+
+    def run(self, stream: cbor.CBORStream.CBORStream, handler):
+        handler('')
+        return [cbor.MajorType.MajorType(), TextStringInf()]
