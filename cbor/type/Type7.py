4332399c4c1656b51f7ad79476424dfda83ac0ad
tnagy <tnagy@rdisoftware.com>
2017-05-03 10:21:26 +0200

    Type7 fix
    yesterday's commit

--- cbor/type/Type7.py
+++ cbor/type/Type7.py
@@ -1,5 +1,7 @@
-from cbor.CBORStream import CBORStream
-from cbor.State import State
+import cbor.CBORStream
+import cbor.MajorType
+import cbor.State
+import struct
 
 ADDITIONAL_INFORMATION_MASK = 0b00011111
 
@@ -13,7 +15,7 @@ inf_end = 31
 def decode_simple_value(value):
     if value <= 19:
         # unassigned
-        return 'pass'
+        return []
     elif value == 20:
         return 'False'
     elif value == 21:
@@ -22,67 +24,67 @@ def decode_simple_value(value):
         return 'Null'
     elif value == 23:
         # undefined
-        return 'pass'
+        return []
     elif ((value >= 24) & (value <= 31)):
         # reserved
-        return 'pass'
+        return []
     else:
         # unassigned
-        return 'pass'
+        return []
 
 
-class Type7Info(State):
+class Type7Info(cbor.State.State):
 
-    def run(self, stream: CBORStream, handler = None):
+    def run(self, stream: cbor.CBORStream, handler):
         current = stream.read(1)
         current = int.from_bytes(current, byteorder='big')
 
         add_inf = (current & ADDITIONAL_INFORMATION_MASK)
 
         if add_inf < 24:
-            simple_value = decode_simple_value(add_inf)
-            handler(simple_value)
-            return None
+            handler(str(add_inf))
+            return []
         elif add_inf == simple_value_next:
-            return Type7Read()
+            return [Type7Read()]
         elif add_inf == float_2byte_next:
-            return FloatRead(2)
+            return [FloatRead(2)]
         elif add_inf == float_4byte_next:
-            return FloatRead(4)
+            return [FloatRead(4)]
         elif add_inf == float_8byte_next:
-            return FloatRead(8)
+            return [FloatRead(8)]
         elif add_inf == inf_end:
-            return 'break'
-
-    def type(self):
-        raise NotImplementedError
+            return ['break']
 
 
-class Type7Read(State):
+class Type7Read(cbor.State.State):
 
-    def run(self, stream: CBORStream, handler = None):
+    def run(self, stream: cbor.CBORStream, handler):
         value = stream.read(1)
         value = int.from_bytes(value, byteorder='big')
 
         simple_value = decode_simple_value(value)
         handler(simple_value)
-        return None
-
-    def type(self):
-        raise NotImplementedError
+        #self.none = None
+        return []
 
 
-class FloatRead(State):
+class FloatRead(cbor.State.State):
+
+    def __eq__(self, other):
+        return self.n == other.n
 
     def __init__(self, n):
-        self.bytes_to_read = n
+        self.n = n
 
-    def run(self, stream: CBORStream, handler = None):
-        value = stream.read(self.bytes_to_read)
-        int_value = int.from_bytes(value, byteorder='big')
-        float_value = float(int_value, 0)
+    def run(self, stream: cbor.CBORStream, handler):
+        value = stream.read(self.n)
+        if self.n == 2:
+            float_value = struct.unpack('e', value)
+        if self.n == 4:
+            float_value = struct.unpack('f', value)
+        if self.n == 8:
+            float_value = struct.unpack('d', value)
+        #int_value = int.from_bytes(value, byteorder='big')
+        #float_value = float(int_value, 0)
         handler(float_value)
-        return None
-
-    def type(self):
-        raise NotImplementedError
+        return []
--- tests/test_Type7.py
+++ tests/test_Type7.py
@@ -1,34 +1,97 @@
-import os
-import sys
-from cbor.Type7 import *
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
-
-from io import RawIOBase, BytesIO
+from io import BytesIO
 from cbor.MajorType import MajorType
 from cbor.CBORStream import CBORStream
+from cbor.type.Tag import TagInfo, TagRead
+from tests.MockHandler import MockHandler
+
+from cbor.type.Type7 import *
+
+#sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
 
 
-def assertHandler(value):
-    def handler(v):
-        assert v == value
-    return handler
+def ignore_handler(v):
+    return
 
+def test_run_type7_probe():
+    data = CBORStream(BytesIO(bytes([0b11100001])))
+    assert type(MajorType().run(data, None)) == Type7Info
 
-def test_Type7Info():
+def test_Type7Info_less_then_24():
+    handler = MockHandler()
+    data = CBORStream(BytesIO(bytes([0b11100011])))
+    stack = Type7Info().run(data, handler.handler)
+    assert len(stack) == 0
+    handler.assert_data('3')
+
+def test_Type7Info_simple_value_next():
     data = CBORStream(BytesIO(bytes([0b11111000])))
-    assert type(Type7Info().run(data, None)) == Type7Read
 
+    stack = Type7Info().run(data, ignore_handler)
+    assert len(stack) == 1
+    assert type(stack[0]) == Type7Read
+
+def test_Type7Info_float_2byte_next():
+    data = CBORStream(BytesIO(bytes([0b11111001])))
+    stack = Type7Info().run(data, ignore_handler)
+    assert len(stack) == 1
+    assert type(stack[0]) == FloatRead
+    assert stack[0] == FloatRead(2)
+
+def test_Type7Info_float_4byte_next():
+    data = CBORStream(BytesIO(bytes([0b11111010])))
+    stack = Type7Info().run(data, ignore_handler)
+    assert len(stack) == 1
+    assert type(stack[0]) == FloatRead
+    assert stack[0] == FloatRead(4)
+
+def test_Type7Info_float_8byte_next():
+    data = CBORStream(BytesIO(bytes([0b11111011])))
+    stack = Type7Info().run(data, ignore_handler)
+    assert len(stack) == 1
+    assert type(stack[0]) == FloatRead
+    assert stack[0] == FloatRead(8)
+
+def test_Type7Info_inf_end():
     data = CBORStream(BytesIO(bytes([0b11111111])))
-    assert Type7Info().run(data, None) == 'break'
+    stack = Type7Info().run(data, ignore_handler)
+    assert len(stack) == 1
+    assert stack[0] == 'break'
 
+def test_Type7Info_inf_end_vmi():
     data = CBORStream(BytesIO(bytes([0b11111001])))
     result = Type7Info().run(data)
     assert type(result) == FloatRead
     assert result.bytes_to_read == 2
 
+def test_Type7Read_True():
+    handler = MockHandler()
+    data = CBORStream(BytesIO(bytes([0b00010101])))
+    stack = Type7Read().run(data, handler.handler)
+    assert len(stack) == 0
+    handler.assert_data('True')
 
-def test_Type7Read():
-    data = CBORStream(BytesIO(bytes([0b11110100])))
-    handler = assertHandler('pass')
-    result = Type7Read().run(data, handler)
-    assert result is None
+def test_Type7Read_False():
+    handler = MockHandler()
+    data = CBORStream(BytesIO(bytes([0b00010100])))
+    stack = Type7Read().run(data, handler.handler)
+    assert len(stack) == 0
+    handler.assert_data('False')
+
+def test_Type7Read_Null():
+    handler = MockHandler()
+    data = CBORStream(BytesIO(bytes([0b00010110])))
+    stack = Type7Read().run(data, handler.handler)
+    assert len(stack) == 0
+    handler.assert_data('Null')
+
+def test_Type7Read_pass():
+    data = CBORStream(BytesIO(bytes([0b00000110])))
+    stack = Type7Read().run(data, ignore_handler)
+    assert len(stack) == 0
+
+#def test_FloatRead():
+#    handler = MockHandler()
+#    data = CBORStream(BytesIO(bytes([0b01000001010001001100110011001101])))
+#    stack = FloatRead(8).run(data, handler.handler)
+#    assert len(stack) == 0
+#    handler.assert_data('12.3')
