4632c1b28e171e6a070e1956b4dd7277791e70b5
gellert <gellihegyi@gmail.com>
2017-05-05 08:47:11 +0200

    adds example

--- examples/simple.py
+++ examples/simple.py
@@ -7,11 +7,22 @@ from cbor.Decoder import Decoder
 
 
 def printer(data):
-    print("Received:", data)
+    print(data, end='')
 
 
 if __name__ == '__main__':
     d = Decoder()
-    data = bytes(3)
-    print(BytesIO(data))
-    print("Decoded:", d.decode_array(data, printer))
+
+    data = bytearray.fromhex('A163617364A26161F46162836178FB 3FF3D70A3D70A3D7A1617A63717171')
+
+    print('')
+
+    d.decode_array(data, printer)
+
+    print('')
+
+# A163617364A26161A0616263636465
+# A163617364A26161A061628361786179A1617A63717171
+# A26161830102636162636162A163666F6F83026461616161A163626172395B9F
+# 830182020363616263
+# A163617364A26161F46162836178FB 3FF3D70A3D70A3D7A1617A63717171
